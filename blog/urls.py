52910ec8b27b09fb61f53e6fff5abec374ea10a5
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-11 16:25:07 +0900

    :sparkles: 詳細ページの実装

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog.views import PostList, PostDetail
 
 urlpatterns = [
     path("", PostList.as_view(), name="post_list"),
-    path("post/<int:pk>", PostDetail.as_view(), name="post_detail"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail"),
 ]
