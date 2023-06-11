ab478117183786c048ade2029de3342027cc9483
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-11 17:26:38 +0900

    :seedling: 下書き設定用の非公開フラグの準備

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.title
