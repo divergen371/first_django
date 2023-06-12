3bcf21027ac57477e5f42236b6e4d02d148ab5f9
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-12 10:20:50 +0900

    :sparkles: イメージカラムを追加

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
